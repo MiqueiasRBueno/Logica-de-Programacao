26d4f2c4d261c07855a183fe9219c26c05fae4c2
MiqueiasRBueno <miqueiasricardobueno@gmail.com>
2025-12-08 17:00:32 -0300

    Adicionado comentário ao código

--- ws-py/Lista_1/02-PERIMETRO_DE_UM_QUADRADO.py
+++ ws-py/Lista_1/02-PERIMETRO_DE_UM_QUADRADO.py
@@ -1,2 +1,4 @@
+# Calcula o perímetro de um quadrado
+
 med_lado_quad = float(input("Digite a medido de um lado da moldura: "))
 print(f"O perímetro da moldura é de \033[31m{med_lado_quad * 4:.2f}\033[m\033[1;34mm/linear\033[m.")
---
+++ ws-py/Lista_1/03-CELSIUS_PARA_FAHRENHEI.py
@@ -0,0 +1,5 @@
+# Converte uma temperatura em Celsius para Fahrenheit
+
+temp_celsius = float(input("Digite a temperatura desejada: "))
+print(f"Temperatura em Celsius : {temp_celsius:.2f}"
+      f"\nTemperatura em Fahrenheit: {(temp_celsius * 1.8) + 32:.2f}")
