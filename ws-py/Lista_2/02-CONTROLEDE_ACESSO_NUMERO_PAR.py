63541f8b43e383694277135c5800a1e104be6d86
MiqueiasRBueno <miqueiasricarbueno@gmail.com>
2025-12-10 16:14:32 -0300

    Código final controle de acesso

--- ws-py/Lista_2/02-CONTROLEDE_ACESSO_NUMERO_PAR.py
+++ ws-py/Lista_2/02-CONTROLEDE_ACESSO_NUMERO_PAR.py
@@ -1,3 +1,7 @@
 # Programa recebe um número e informa se ele é par ou ímpar.
 
 cod_acesso = int(input('Insira seu código: '))
+if cod_acesso % 2 == 0:
+    print('Número par, \033[34macesso liberado!\033[m')
+else:
+    print('Número impar, \033[31macesso negado!\033[m')
