6ab76df6a8059e0eb845102b0ed16fe8642b864f
MiqueiasRBueno <miqueiasricarbueno@gmail.com>
2025-12-12 14:30:44 -0300

    Arquivo inicial ex05 lista 2 em Python.

--- ws-py/Lista_2/05-RESULTADO_AVALIACAO_ESCOLAR.py
+++ ws-py/Lista_2/05-RESULTADO_AVALIACAO_ESCOLAR.py
@@ -1,9 +1,18 @@
 # Programa recebe uma nota e informa se o aluno foi aprovado
 
-nota_aluno = float(input('Insira a nota do aluno: '))
-if nota_aluno >= 7:
-    print(f'Aluno tirou nota {nota_aluno}, aluno aprovado!')
-elif 5 < nota_aluno < 7:
-    print(f'Aluno tirou nota {nota_aluno}, aluno de recuperação!')
-else:
-    print(f'Aluno tirou nota {nota_aluno}, reprovado!')
+while True:
+    try:
+        nota_aluno = float(input('Insira a nota do aluno: '))
+        if nota_aluno >= 7:
+            print(f'Aluno tirou nota {nota_aluno}, aluno aprovado!')
+        elif 5 < nota_aluno < 7:
+            print(f'Aluno tirou nota {nota_aluno}, aluno de recuperação!')
+        else:
+            print(f'Aluno tirou nota {nota_aluno}, reprovado!')
+    except ValueError:
+        print("Erro, digite somente valor númerico!")
+    parar = ' '
+    while parar not in "SN":
+        parar = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    if parar == "N":
+        break
