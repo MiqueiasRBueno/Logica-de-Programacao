a2053dc86669e1d1442bfbba1b8757386ad4c5b9
MiqueiasRBueno <miqueiasricarbueno@gmail.com>
2025-12-15 13:33:39 -0300

    Realocando arquivo

---
+++ ws-py/calculadora/calc.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+# ==============================
+# Configurações iniciais
+# ==============================
+
+ctk.set_aparence_mod("dark")
+ctk.set_default_color_teme("blue")
